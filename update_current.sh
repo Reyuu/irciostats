2b4b3a31daaea64c186b50fcb4bbfcf8aa27b25a
Reyuu <rey444xd3@gmail.com>
2017-04-13 16:44:19 +0200

    added update scripts and corresponding config files

---
+++ update_current.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/rey44
+array=( "polish" "german" "english" "japanese" "indonesian" "ctb" "taiko" "osumania" "modhelp" "hungarian" "chinese" "korean" "french" "russian" "spanish" "dutch" "videogames" "modreqs" "help" "finnish" "skandinavian" "italian" "filipino" "romanian" "czechoslovak" "cantonese" "hebrew" "malaysian" "balkan" "bulgarian" "arabic" "turkish" "portuguese" "greek" "thai" )
+for i in "${array[@]}"
+do
+	python /home/rey44/irciostats/main.py /home/rey44/irciostats/current.cfg \#$i ;
+done
